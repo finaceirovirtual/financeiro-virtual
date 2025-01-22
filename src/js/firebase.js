1cd4372fb3104b6fd5f07f2452b1bb9051f9005f
Financerio Virtual <finanirovirtualsuporte@gmail.com>
2025-01-22 17:34:32 -0300

    firebase firestore

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -2,21 +2,24 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile, signOut, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, collection, addDoc, doc, setDoc, getDocs } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
-// Configuração do Firebase
+// Configuração do Firebase (use apenas uma configuração)
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_AUTH_DOMAIN",
-    projectId: "SEU_PROJECT_ID",
-    storageBucket: "SEU_STORAGE_BUCKET",
-    messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-    appId: "SEU_APP_ID"
+  apiKey: "AIzaSyD_K7ESDO4c9ja_mFNF2RGjx7KOxWPzuXo",
+  authDomain: "financas-8cf44.firebaseapp.com",
+  projectId: "financas-8cf44",
+  storageBucket: "financas-8cf44.appspot.com",
+  messagingSenderId: "637122171830",
+  appId: "1:637122171830:web:8c42d2169f6176cb0d6717",
+  measurementId: "G-GZJSG5TMLS"
 };
 
 // Inicialize o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const firestore = getFirestore(app);
+const analytics = getAnalytics(app); // Firebase Analytics (opcional)
 
 // Exporte as funções necessárias
 export {
@@ -33,5 +36,6 @@ export {
     addDoc,
     doc,
     setDoc,
-    getDocs
+    getDocs,
+    analytics // Exporte o analytics se for usar
 };
