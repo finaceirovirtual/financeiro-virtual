6b8ac1fe2b3a514c309a19199ba745bb36cae90f
Financerio Virtual <finanirovirtualsuporte@gmail.com>
2025-01-22 18:16:24 -0300

    Descrição das alterações

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,8 +1,41 @@
+// Importações do Firebase
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-} from './firebase.js'; // Caminho relativo
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  updateProfile,
+  signOut,
+  onAuthStateChanged
+} from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  doc,
+  setDoc,
+  getDocs
+} from "firebase/firestore";
 
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD_K7ESDO4c9ja_mFNF2RGjx7KOxWPzuXo",
+  authDomain: "financas-8cf44.firebaseapp.com",
+  projectId: "financas-8cf44",
+  storageBucket: "financas-8cf44.appspot.com",
+  messagingSenderId: "637122171830",
+  appId: "1:637122171830:web:8c42d2169f6176cb0d6717",
+  measurementId: "G-GZJSG5TMLS"
+};
+
+// Inicialize o Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+// Restante do código...
 document.getElementById('form-cadastro').addEventListener('submit', function (event) {
   event.preventDefault(); // Impede o envio do formulário
 
